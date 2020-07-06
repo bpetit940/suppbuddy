5b264fdd61fc742be9c236a0e39a2caa1bb80f10
Brannen Petit <bpetit940@gmail.com>
2020-07-05 20:00:31 -0500

    started on quiz

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Router } from "@reach/router";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
+import Quiz from "./Quiz";
 import LandingPage from "./LandingPage";
 import { UserContext } from "../providers/UserProvider";
 import ProfilePage from "./ProfilePage";
@@ -17,6 +18,7 @@ function Application() {
       <SignUp path="signUp" />
       <SignIn path="signIn" />
       <PasswordReset path="passwordReset" />
+      <Quiz path="quiz" />
     </Router>
   );
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,14 +2,25 @@ import React, { useContext, Component } from "react";
 import { UserContext } from "../providers/UserProvider";
 import { auth } from "../firebase";
 class Quiz extends Component {
-
   render() {
     return (
       <div className="quiz-container">
         <h1>Supplement Personalization Quiz</h1>
+        <form>
+          <label>Question 1</label>
+          <input type="radio" value="Option" />
+          <label>Option</label>
+          <input type="radio" value="Option" />
+          <label>Option</label>
+          <input type="radio" value="Option" />
+          <label>Option</label>
+          <input type="radio" value="Option" />
+          <label>Option</label>
+          <input type="radio" value="Option" />
+          <label>Option</label>
+        </form>
       </div>
     );
   }
-
-};
+}
 export default Quiz;
